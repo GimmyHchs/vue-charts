9ecb4e411f18325685c25ce6876b00e5fe5713a7
g9308370 <g9308370@hotmail.com>
2016-10-12 18:29:39 +0800

    create picture

--- vue-charts.js
+++ vue-charts.js
@@ -1,4 +1,6 @@
 if(Vue === undefined)
     throw "Vue is undefined";
+    
+window.Chart = require('chart.js');
 window.vue_charts = require('./vue-chartjs-lib.js');
 Vue.component('chartjs-line', require('./components/chartjs-line.vue'));
